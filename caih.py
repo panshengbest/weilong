f56d2d0ac273aa2bcec80dc5b7eccf059b429a26
panshengbest <91641828+panshengbest@users.noreply.github.com>
2023-04-22 15:16:42 +0800

    Add files via upload

---
+++ caih.py
@@ -0,0 +1,40 @@
+from helium import *
+import time
+
+def check_in(username, password):
+    # 打开网页
+    start_chrome('https://720ccl88.emy163.cn/user/login.php')
+
+    # 等待页面加载
+    wait_until(Text('立即登陆').exists)
+
+    # 输入用户名和密码
+    write(username, into=TextField('用户名'))
+    write(password, into=TextField('密码'))
+
+    # 点击登录按钮
+    click(Button('立即登陆'))
+
+    # 等待页面加载
+    wait_until(Text('系统管理中心').exists)
+
+    # 点击签到按钮
+    click(Text('每日签到'))
+    wait_until(Text('总奖励').exists)
+    click(S('#qiandao'))
+
+    # 等待签到成功提示出现
+    wait_until(Text('签到成功').exists)
+
+    # 关闭浏览器标签页
+    kill_browser()
+
+# 读取账号密码文件
+with open('accounts.txt', 'r') as f:
+    accounts = f.readlines()
+
+# 循环调用签到函数
+for account in accounts:
+    username, password = account.strip().split(',')
+    check_in(username, password)
+    time.sleep(5) # 每次签到之间暂停5秒

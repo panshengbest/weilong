5e89c3b5ef573cbcbb7897c62120e8a347901b15
panshengbest <91641828+panshengbest@users.noreply.github.com>
2023-04-26 12:36:15 +0800

    Update caih.py

--- caih.py
+++ caih.py
@@ -3,38 +3,38 @@ import time
 
 def check_in(username, password):
     try:
-    # 打开网 页
-    start_chrome('https://720ccl88.emy163.cn/user/login.php')
+         # 打开网 页
+         start_chrome('https://720ccl88.emy163.cn/user/login.php')
 
-    # 等待页面加载
-    #wait_until(Text('立即登陆').exists)
-    time.sleep(15)
+         # 等待页面加载
+         #wait_until(Text('立即登陆').exists)
+         time.sleep(15)
 
-    # 输入用户名和密码
-    write('username',S('/html/body/div[1]/div/div[2]/form/div[1]/input'))
-    write(password, into=TextField('密码'))
+         # 输入用户名和密码
+         write('username',S('/html/body/div[1]/div/div[2]/form/div[1]/input'))
+         write(password, into=TextField('密码'))
 
-    # 点击登录按钮
-    click(Button('立即登陆'))
+         # 点击登录按钮
+         click(Button('立即登陆'))
 
-    # 等待页面加载
-    #wait_until(Text('系统管理中心').exists)
-    time.sleep(15)
+         # 等待页面加载
+         #wait_until(Text('系统管理中心').exists)
+         time.sleep(15)
 
-    # 点击签到按钮
-    click(Text('每日签到'))
-    #wait_until(Text('总奖励').exists)
-    time.sleep(15)
-    click(S('#qiandao'))
+         # 点击签到按钮
+         click(Text('每日签到'))
+         #wait_until(Text('总奖励').exists)
+         time.sleep(15)
+         click(S('#qiandao'))
 
-    # 等待签到成功提示出现
-    #wait_until(Text('签到成功').exists)
-    time.sleep(15)
+         # 等待签到成功提示出现
+         #wait_until(Text('签到成功').exists)
+         time.sleep(15)
 
-    # 关闭浏览器标签页
-    kill_browser()
-except Exception as e:
-    print("错求了:", e)
+         # 关闭浏览器标签页
+         kill_browser()
+    except Exception as e:
+         print("错求了:", e)
 
 # 读取账号密码文件
 with open('accounts.txt', 'r') as f:
